27997bb72a8fe03fdaa2513049b766098976a846
Robert Joosten <rwm.joosten@gmail.com>
2017-10-08 23:56:55 +0100

    documentation

--- __init__.py
+++ __init__.py
@@ -1,26 +1,42 @@
-"""					
-I N S T A L L A T I O N:
-    Copy the "rjReorderAttr" folder to your Maya scripts directory:
-        C:\Users\<USER>\Documents\maya\scripts
+"""		
+Reorder attributes in Maya.
 
-U S A G E:
-    Add functionality to the Channel Box -> Edit menu in Maya:
-        import maya.cmds as cmds
-        cmds.evalDeferred("import rjReorderAttr; rjReorderAttr.install()")
+.. figure:: https://github.com/robertjoosten/rjReorderAttr/raw/master/README.png
+   :align: center
+   
+`Link to Video <https://vimeo.com/210495749>`_
+
+Installation
+============
+Copy the **rjReorderAttr** folder to your Maya scripts directory
+::
+    C:/Users/<USER>/Documents/maya/scripts
+
+Usage
+=====
+Add functionality to the Channel Box -> Edit menu in Maya
+::
+    import maya.cmds as cmds
+    cmds.evalDeferred("import rjReorderAttr; rjReorderAttr.install()")
     
-    Display UI:  
-        import rjReorderAttr.ui 
-        rjReorderAttr.ui.show()
+Display UI
+::
+    import rjReorderAttr.ui 
+    rjReorderAttr.ui.show()
 
-N O T E:
-    If the install command is used a button called Reorder Attributes will be
-    added to the Channel Box -> Edit menu. If this is not the case the ui can
-    be opened with the show command. Drag and drop the attributes to reorder.
-    Attributes are deleted in the new order and the undo commands is then ran
-    to redo the attributes in the order prefered.
+Note
+====
+If the install command is used a button called Reorder Attributes will be
+added to the Channel Box -> Edit menu. If this is not the case the ui can
+be opened with the show command. Drag and drop the attributes to reorder.
+Attributes are deleted in the new order and the undo commands is then ran
+to redo the attributes in the order prefered.
 
-    A thank you too Nick Hughes for showing me the power of the undo command 
-    and how it can be used to sort attributes.
+A thank you too Nick Hughes for showing me the power of the undo command 
+and how it can be used to sort attributes.
+
+Code
+====
 """
 
 from maya import cmds, mel
